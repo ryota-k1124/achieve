f69f276af84f926cf58c04e183201d496e9c5fe1
ryota <ryota-k@nttpc.co.jp>
2018-01-07 13:14:58 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'secret-retreat-62764.herokuapp.com' }
+  #config.action_mailer.default_url_options = { host: 'secret-retreat-62764.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'frozen-temple-23071.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
