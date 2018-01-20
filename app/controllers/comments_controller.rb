d0e860a07263ad757c254c1e38cc46530dd7ab25
ryota <ryota-k@nttpc.co.jp>
2018-01-20 07:41:21 +0000

    space del

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
         
         unless @comment.blog.user_id == current_user.id
           Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
-          　message: 'あなたの作成したブログにコメントが付きました'
+            message: 'あなたの作成したブログにコメントが付きました'
           })
           Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
             unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count
