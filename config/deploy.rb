bd8b53567951b7cb79ae6a87b7e1a8b1bfc74f62
ryota <ryota-k@nttpc.co.jp>
2018-01-21 07:35:02 +0000

    fix giturl

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'achieve'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/ryota-k1124/achieve'
+set :repo_url, 'https://github.com/ryota-k1124/achieve.git'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
